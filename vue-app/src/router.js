ecbb7d040e8a2f69cc43a01b8acb1d1c576b178b
Funbek <web_tec@mail.ru>
2018-09-26 22:20:42 +0300

    third-task + Fix some bugs

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "@/views/Home.vue";
 import Users from "@/views/Users.vue";
 import EditUser from "@/views/EditUser.vue";
 
