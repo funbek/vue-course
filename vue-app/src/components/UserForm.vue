8edcdf33a57dddeb6f735e68eff0ca7a9b59e51f
d.radchenko <dradchenko@ourgapps.com>
2018-10-03 15:33:33 +0300

    fourth-task + Add user with popup

--- vue-app/src/components/UserForm.vue
+++ vue-app/src/components/UserForm.vue
@@ -2,35 +2,117 @@
   <div>
     <div class="form-group">
       <label>Имя</label>
-      <input 
-        v-model="user.firstName" 
-        type="text" 
+      <input
+        v-model="user.firstName"
+        type="text"
         class="form-control" >
     </div>
 
     <div class="form-group">
       <label>Фамилия</label>
-      <input 
-        v-model="user.lastName" 
-        type="text" 
+      <input
+        v-model="user.lastName"
+        type="text"
         class="form-control" >
     </div>
 
     <div class="form-group">
+      <label>Email</label>
+      <input
+        v-model="user.email"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>Url картинки</label>
+      <input
+        v-model="user.picture"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>Возраст</label>
+      <input
+        v-model="user.age"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>Активный</label>
+      <input
+        v-model="user.isActive"
+        type="checkbox"
+        class="checkbox-inline" >
+    </div>
+
+    <div class="form-group">
       <label>Баланс</label>
-      <input 
-        v-model="user.balance" 
-        type="text" 
+      <input
+        v-model="user.balance"
+        type="text"
         class="form-control" >
     </div>
 
     <div class="form-group">
       <label>Телефон</label>
-      <input 
-        v-model="user.phone" 
-        type="text" 
+      <input
+        v-model="user.phone"
+        type="text"
         class="form-control" >
     </div>
+
+    <div class="form-group">
+      <label>Уровень доступа</label>
+      <select
+        v-model="user.accessLevel"
+        class="form-control"
+      >
+        <option
+          v-for="item in accessList"
+          :key="item"
+          :value="item"
+        >
+          {{ item }}
+        </option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label>Адрес</label>
+      <input
+        v-model="user.address"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>Компания</label>
+      <input
+        v-model="user.company"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>О пользователе</label>
+      <input
+        v-model="user.about"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <div class="form-group">
+      <label>Дата регистрации</label>
+      <input
+        v-model="user.registered"
+        type="text"
+        class="form-control" >
+    </div>
+
+    <slot name="buttons" />
 
     <div>
       <pre>{{ user }}</pre>
@@ -42,11 +124,17 @@
 <script>
 export default {
   name: "UserForm",
+  model: {
+    prop: "user"
+  },
   props: {
     user: {
       type: Object,
       required: true
     }
-  }
+  },
+  data: () => ({
+    accessList: ["user", "admin", "guest"]
+  })
 };
 </script>
--- vue-app/src/views/AddUser.vue
+++ vue-app/src/views/AddUser.vue
@@ -2,17 +2,42 @@
   <div>
     <h2>Добавление пользователя</h2>
 
-    <user-form 
-      v-if="user"
-      :user="user"
-      @input="value => user = value" />
-    <div v-else>Пользователей нет :(</div>
+    <user-form
+      v-model="user"
+    >
+      <div slot="buttons">
+        <button
+          type="button"
+          class="btn btn-success"
+          @click="save"
+        >
+          Добавить пользователя
+        </button>
+      </div>
+    </user-form>
+
+    <template v-if="popupShow">
+      <div class="popup-shadow" />
+      <div
+        class="popup"
+      >
+        <h2>Поздравляем, пользователь добавлен</h2>
+
+        <button
+          class="btn btn-success"
+          @click="agreePopup"
+        >
+          Ok
+        </button>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 // import axios from "axios";
 import UserForm from "@/components/UserForm.vue";
+import axios from "axios";
 
 const emptyObj = {
   id: 0,
@@ -36,15 +61,47 @@ export default {
     UserForm
   },
   data: () => ({
-    user: null
+    user: emptyObj,
+    url: "http://localhost:3004/users",
+    popupShow: false
   }),
-  mounted() {
-    this.loadData();
-  },
   methods: {
-    loadData() {
-      this.user = Object.assign({}, emptyObj);
+    save() {
+      axios
+        .post(this.url, this.user)
+        .then(() => {
+          this.popupShow = true;
+        })
+        .catch(err => {
+          console.log("this is error: " + err);
+        });
+    },
+    agreePopup() {
+      this.$router.push({ path: "/users" });
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.popup {
+  position: fixed;
+  z-index: 2;
+  width: 600px;
+  height: 400px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  top: 50%;
+  left: 50%;
+  background: #fff;
+  transform: translate(-50%, -50%);
+}
+.popup-shadow {
+  position: fixed;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
